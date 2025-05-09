779fc7da3eb16354560f8c80c51469f6479368dc
mannuccimattia <mannuccimattia@gmail.com>
2025-05-09 14:43:01 +0200

    [UPD] Done static array

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import { useState } from 'react'
 
+const todo = [
+  "Leggere la documentazione",
+  "Fare l'esercizio del giorno",
+  "Fare una pausa",
+  "Pulire casa",
+  "Fare la spesa",
+  "Cenare",
+  "Loggare in orario per il raid"
+];
 
 const App = () => {
   return (
-    <></>
+    <>
+      <div className="container m-4">
+        <h1>To Do List</h1>
+        <ul className="list-group my-4">
+          {todo.map((task, index) => (
+            <li
+              key={index}
+              className='list-group-item'
+            >
+              {task}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   )
 }
 
