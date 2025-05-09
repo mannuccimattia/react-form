2cd226bcd98bcb68add47493a9b3c4878c7c1518
mannuccimattia <mannuccimattia@gmail.com>
2025-05-09 15:13:09 +0200

    [UPD] Done static delete button

--- src/App.jsx
+++ src/App.jsx
@@ -30,9 +30,13 @@ const App = () => {
           {todoList.map((task, index) => (
             <li
               key={index}
-              className='list-group-item'
+              className='list-group-item d-flex justify-content-between align-items-center'
             >
               {task}
+              <button
+                className='btn btn-danger ms-auto'>
+                Cancella
+              </button>
             </li>
           ))}
         </ul>
